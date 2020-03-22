9d535e800fd5e2bbb9e9d7103c7eaa5a4e44b99b
wax <494672409@qq.com>
2020-03-22 20:20:05 +0800

    es搜索页面

--- src/main/java/com/wax/blogsystem/controller/BlogEsController.java
+++ src/main/java/com/wax/blogsystem/controller/BlogEsController.java
@@ -2,11 +2,15 @@ package com.wax.blogsystem.controller;
 
 import com.wax.blogsystem.common.JSONUtil;
 import com.wax.blogsystem.domain.BlogEs;
+import com.wax.blogsystem.domain.User;
 import com.wax.blogsystem.service.BlogEsService;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,11 +21,27 @@ public class BlogEsController {
     @Autowired
     private BlogEsService blogEsService;
 
-    @RequestMapping(value = "/allSearch.do")
-    @ResponseBody
-    public String searchAll(String str, Pageable pageable){
+    @RequestMapping(value = "/goAllSearch.do")
+    public String goAllSearch(Model model){
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        model.addAttribute("loginUser",user);
+        return "/search/allSearch";
+    }
+
+    @RequestMapping(value = "/goSearchList.do")
+    public String goSearchList(Model model,String str){
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        model.addAttribute("loginUser",user);
+        model.addAttribute("searchStr",str);
+        return "/search/list";
+    }
+
+    @PostMapping(value = "/allSearch.do")
+    public String searchAll(String str, Pageable pageable,Model model){
         Page<BlogEs> page =blogEsService.allSearch(str,pageable);
-        return JSONUtil.result(true,page.getContent(),"");
+        model.addAttribute("searchList",page.getContent());
+        model.addAttribute("searchTotalNum",page.getTotalElements());
+        return "search/list::search_list";
     }
 
     @RequestMapping(value = "/addOne.do")
